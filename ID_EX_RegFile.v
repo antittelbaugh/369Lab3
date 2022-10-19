780673f4e73f74a5bb5a0c36cc99e6082ae3c915
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2022-10-19 14:56:09 -0700

    Update ID_EX_RegFile.v

--- ID_EX_RegFile.v
+++ ID_EX_RegFile.v
@@ -10,13 +10,13 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module IDEXReg(Clk, ID_WB_Ctrl, ID_MEM_Ctrl,ID_EX_Ctrl, ID_PCAddResult, ID_SignExtend,ID_SignExtend_10_6, ID_Read1, ID_Read2, ID_Instruction16_20, ID_Instruction5_11, EX_WBCtrl, EX_MEMCtrl, EX_RegDst, EX_ALUOp, EX_ALUSrc, EX_halfbyte, EX_PCAddResult, EX_Read1, EX_Read2, EX_SignExtend,EX_SignExtend_10_6,EX_Instruction16_20, EX_Instruction5_11);
+module IDEXReg(Clk, ID_WB_Ctrl, ID_MEM_Ctrl, ID_PCAddResult, ID_EX_Ctrl, ID_SignExtend, ID_SignExtend_10_6, ID_Read1, ID_Read2, ID_Instruction16_20, ID_Instruction5_11, EX_WBCtrl, EX_MEMCtrl, EX_RegDst, EX_ALUOp, EX_ALUSrc, EX_halfbyte, EX_PCAddResult, EX_Read1, EX_Read2, EX_SignExtend,EX_SignExtend_10_6,EX_Instruction16_20, EX_Instruction5_11);
     
     input Clk;
     input [3:0] ID_WB_Ctrl;
-    input [4:0] ID_MEM_Ctrl;
-    input [8:0] ID_EX_Ctrl;
-    input [31:0] ID_PCAddResult, ID_SignExtend_10_6, ID_SignExtend, ID_Read1, ID_Read2;
+    input  [4:0] ID_MEM_Ctrl;
+    input  [8:0] ID_EX_Ctrl;
+    input  [31:0] ID_PCAddResult, ID_SignExtend_10_6, ID_SignExtend, ID_Read1, ID_Read2;
     input [4:0]  ID_Instruction16_20, ID_Instruction5_11;
 
     output reg [3:0] EX_WBCtrl;
@@ -46,7 +46,7 @@ always @(posedge Clk) begin
     EX_SignExtend <= ID_SignExtend;
     EX_SignExtend_10_6 <= ID_SignExtend_10_6;
     EX_Instruction16_20 <= ID_Instruction16_20;
-    EX_Instruction_5_11 <= ID_Instruction5_11;
+    EX_Instruction5_11 <= ID_Instruction5_11;
 
  end
 endmodule
