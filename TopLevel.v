1508d1a83abc584ffd2753f95609b2e3ee4a2e53
mariow-ops <104529480+mariow-ops@users.noreply.github.com>
2022-10-19 14:22:02 -0700

    Create TopLevel.v

---
+++ TopLevel.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module TopLevel();
+    input Reset, Clk;
+    output [6:0] out7;
+    output wire [7:0] en_out;
+    wire ClkOut;
+    wire [31:0] Instruction;
+    
+    ClkDiv clk(Clk, Reset, ClkOut);
+    
+endmodule
