346f28cc1137e70d73fed65e1f5e2c54cc502e34
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2022-10-23 08:59:03 -0700

    Update EX_MEM.v

--- EX_MEM.v
+++ EX_MEM.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResult, EX_WriteMemData, EX_WriteRegData,Clk,M_WB, M_BranchCon,M_MemRead, M_Branch, M_MemWrite, M_BNE,M_PCinc, M_BranchAddResult, M_ZeroFlag, M_ALUResult, M_WriteMemData, M_WriteRegData);
+module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResult, EX_WriteMemData, EX_WriteRegData,Clk, Reset, M_WB, M_BranchCon,M_MemRead, M_Branch, M_MemWrite, M_BNE,M_PCinc, M_BranchAddResult, M_ZeroFlag, M_ALUResult, M_WriteMemData, M_WriteRegData);
     input [3:0] EX_WB;
     input [4:0] EX_M;
     input [31:0] EX_PCinc;
@@ -28,8 +28,9 @@ module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResu
     input EX_ZeroFlag;
     input [31:0] EX_ALUResult;
     input [31:0] EX_WriteMemData;
-    input [31:0] EX_WriteRegData;
+    input [4:0] EX_WriteRegData;
     input Clk;
+    input Reset;
     output reg [3:0] M_WB;
     output reg M_BranchCon;
     output reg M_MemRead;
@@ -41,9 +42,24 @@ module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResu
     output reg M_ZeroFlag;
     output reg [31:0] M_ALUResult;
     output reg [31:0] M_WriteMemData;
-    output reg [31:0] M_WriteRegData;
+    output reg [4:0] M_WriteRegData;
     
-    always @(posedge Clk) begin
+    always @(posedge Clk, Reset) begin
+    if (Reset == 1) begin
+     M_WB <= 0;
+     M_BranchCon <= 0;
+     M_MemRead <= 0;
+     M_Branch<= 0;
+     M_MemWrite<= 0;
+     M_BNE<= 0;
+     M_PCinc <= 0;
+     M_BranchAddResult <= 0;
+     M_ZeroFlag <= 0;
+     M_ALUResult <= 0;
+     M_WriteMemData <= 0;
+     M_WriteRegData <= 0;
+    end
+    else begin
      M_WB <= EX_WB ;
      M_BranchCon <= EX_M[1];
      M_MemRead <= EX_M[3];
@@ -52,10 +68,11 @@ module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResu
      M_BNE<= EX_M[0];
      M_PCinc <= EX_PCinc;
      M_BranchAddResult <= EX_BranchAddResult;
-     M_ZeroFlag <= M_ZeroFlag;
+     M_ZeroFlag <= EX_ZeroFlag;
      M_ALUResult <= EX_ALUResult;
      M_WriteMemData <= EX_WriteMemData;
-     M_WriteRegData <= M_WriteRegData;
+     M_WriteRegData <= EX_WriteRegData;
+    end
     
     end
 endmodule
