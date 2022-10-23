02bb3c2a237eccb097ddb53455879d90b37f1fe9
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2022-10-23 09:23:53 -0700

    Update IF_ID_RegFile.v

--- IF_ID_RegFile.v
+++ IF_ID_RegFile.v
@@ -10,18 +10,24 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module IF_ID_RegFile(Clk, PCAddResult, Instruction, ID_PCAddResult, ID_Instruction);
+module IF_ID_RegFile(Clk, Reset, PCAddResult, Instruction, ID_PCAddResult, ID_Instruction);
     
     input Clk;
+    input Reset;
     input [31:0] PCAddResult, Instruction;
     output reg[31:0] ID_PCAddResult, ID_Instruction;
     
    
 always @(posedge Clk) begin
 
-    
+    if (Reset == 1) begin
+    ID_PCAddResult <= 0;
+    ID_Instruction <= 0;
+    end
+    else begin
     ID_PCAddResult <= PCAddResult;
-    ID_Instruction <= Instruction; 
+    ID_Instruction <= Instruction;
+    end 
     
  end
 endmodule
