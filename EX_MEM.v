92e6eecaeff50268ff9e2da374bf3f1b4867f78d
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2022-10-23 13:30:05 -0700

    Update EX_MEM.v

--- EX_MEM.v
+++ EX_MEM.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResult, EX_WriteMemData, EX_WriteRegData,Clk, Reset, M_WB, M_BranchCon,M_MemRead, M_Branch, M_MemWrite, M_BNE,M_PCinc, M_BranchAddResult, M_ZeroFlag, M_ALUResult, M_WriteMemData, M_WriteRegData);
+module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResult, EX_WriteMemData, EX_WriteRegData,Clk, Reset, M_WB, M_BranchCon,M_MemRead, M_Branch, M_MemWrite, M_BNE,M_PCinc, M_BranchAddResult, M_ZeroFlag, M_ALUResult, M_WriteMemData, M_WriteRegData, EX_jump, EX_offset, EX_Read1, EX_jr, M_jump, M_offset, M_Read1, M_jr);
     input [3:0] EX_WB;
     input [4:0] EX_M;
     input [31:0] EX_PCinc;
@@ -31,6 +31,11 @@ module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResu
     input [4:0] EX_WriteRegData;
     input Clk;
     input Reset;
+   input EX_jump;
+    input [25:0] EX_offset;
+    input [31:0] EX_Read1;
+    input EX_jr;
+    
     output reg [3:0] M_WB;
     output reg M_BranchCon;
     output reg M_MemRead;
@@ -43,9 +48,18 @@ module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResu
     output reg [31:0] M_ALUResult;
     output reg [31:0] M_WriteMemData;
     output reg [4:0] M_WriteRegData;
+    output reg M_jump;
+    output reg [25:0] M_offset;
+    output reg [31:0] M_Read1;
+    output reg M_jr;
     
     always @(posedge Clk, Reset) begin
     if (Reset == 1) begin
+    
+    M_jump <= 0;
+    M_offset <= 0;
+     M_Read1 <= 0;
+     M_jr <= 0;
      M_WB <= 0;
      M_BranchCon <= 0;
      M_MemRead <= 0;
@@ -72,6 +86,11 @@ module EX_MEM(EX_WB, EX_M, EX_PCinc, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResu
      M_ALUResult <= EX_ALUResult;
      M_WriteMemData <= EX_WriteMemData;
      M_WriteRegData <= EX_WriteRegData;
+     
+     M_jump <= EX_jump;
+    M_offset <= EX_offset;
+     M_Read1 <= EX_Read1;
+     M_jr <= EX_jr;
     end
     
     end
