f4171c4d7a3646cfacdcf255a9f00e18ed6a6105
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2022-10-23 08:52:46 -0700

    Update Data_Path.v

--- Data_Path.v
+++ Data_Path.v
@@ -4,14 +4,14 @@
 
 module Data_Path(Reset, Clk);
     input Reset, Clk;
-    
+    (* mark_debug = "true" *) wire [31:0] PCResult;
+    (* mark_debug = "true" *)wire [31:0] WriteData;
     wire [31:0] PCAddResult, Instruction;
-    wire [31:0] EX_PCinc;
     wire [31:0] EX_BranchAddResult;
     wire EX_ZeroFlag;
     wire [31:0] EX_ALUResult;
     wire [31:0] EX_WriteMemData;
-    wire [31:0] EX_WriteRegData;
+    wire [4:0] EX_WriteRegData;
     wire [3:0] ID_WB_Ctrl;
     wire  [4:0] ID_MEM_Ctrl;
     wire  [8:0] ID_EX_Ctrl;
@@ -39,11 +39,13 @@ module Data_Path(Reset, Clk);
     wire [31:0] EX_PCAddResult, EX_Read1, EX_Read2, EX_SignExtend,EX_SignExtend_10_6;
     wire [4:0] EX_Instruction16_20, EX_Instruction5_11;
     wire [3:0] MEM_WB_Ctrl;
-    wire [31:0] MEM_Read, MEM_ALUResult, MEM_RegDst;
+    wire [31:0] MEM_Read;
+    wire [4:0] MEM_RegDst;
     wire WB_halfbyte;
     wire [1:0] WB_MemToReg;
     wire WB_RegWrite;
-    wire [31:0] WB_PCAddResult, WB_Read, WB_ALUResult, WB_RegDst;
+    wire [31:0] WB_PCAddResult, WB_Read, WB_ALUResult;
+    wire [4:0] WB_RegDst;
     
     wire jr;
     wire shift;
@@ -60,18 +62,18 @@ module Data_Path(Reset, Clk);
     wire PCSrc;
     wire [31:0]  loadByte;
     wire [31:0]  loadhalf;
-    wire [31:0] WriteData;
+    
     wire [31:0] Address;       
     wire [31:0] mem [127:0];
-    wire [31:0] Instruction;
-    wire [31:0] PCResult;
     wire [31:0] Address_mux;
     wire [27:0] Shifted_jr;
     wire ID_jump;
     wire [31:0] Read1_Result;
+    wire [31:0] lhlbResult;
+    
     
 
-    Mux32Bit2To1 PCSrc(Address_mux, PCAddResult,  M_BranchAddResult, WB_PCsrc);
+    Mux32Bit2To1 PCSrc1(Address_mux, PCAddResult,  M_BranchAddResult, PCSrc);
     InstructionMemory IMEM(PCResult, Instruction); 
     PCAdder PCA(PCResult, PCAddResult);
     ProgramCounter PCount(Address, PCResult, Reset, Clk);
@@ -82,7 +84,7 @@ module Data_Path(Reset, Clk);
 	
     IF_ID_RegFile IFID(Clk, PCAddResult, Instruction, ID_PCAddResult, ID_Instruction);
 	
-    RegisterFile RegFile1(ID_Instruction[25:21], ID_Instruction[20:16], WB_RegDst, WriteData, WB_RegWrite, ID_Read1, ID_Read2, Clk, Rst);
+    RegisterFile RegFile1(ID_Instruction[25:21], ID_Instruction[20:16], WB_RegDst, WriteData, WB_RegWrite, ID_Read1, ID_Read2, Clk, Reset);
     SignExtension Sign1(ID_Instruction[15:0], ID_SignExtend);
     Controller  Con1(ID_Instruction[31:26],ID_EX_Ctrl, ID_MEM_Ctrl, ID_WB_Ctrl, ID_jump);
     SignExtension_5  SignEX1(ID_Instruction[10:6], ID_SignExtend_10_6);
@@ -91,8 +93,8 @@ module Data_Path(Reset, Clk);
 
     IDEXReg IDEX(Clk, ID_WB_Ctrl, ID_MEM_Ctrl, ID_PCAddResult, ID_EX_Ctrl, ID_SignExtend, ID_SignExtend_10_6, ID_Read1, ID_Read2, ID_Instruction[20:16], ID_Instruction[15:11], EX_WBCtrl, EX_MEMCtrl, EX_RegDst, EX_ALUOp, EX_ALUSrc, EX_halfbyte, EX_PCAddResult, EX_Read1, EX_Read2, EX_SignExtend,EX_SignExtend_10_6,EX_Instruction16_20, EX_Instruction5_11);
         
-    Mux32Bit3To1 REGDST(EX_WriteRegData, EX_Instruction16_20, EX_Instruction5_11,31, EX_RegDst);  
-    ALUControl ALUCON(EX_ALUOp,EX_SignExtend,jr,shift,ALUcon); 
+    Mux3To1_5bit REGDST(EX_WriteRegData, EX_Instruction16_20, EX_Instruction5_11,31, EX_RegDst);
+    ALUControl ALUCON(EX_ALUOp,EX_SignExtend[5:0],jr,shift,ALUCon); 
     Mux32Bit2To1 ALUSrc(ALUSrcResult, EX_Read2, EX_SignExtend, EX_ALUSrc);
     Mux32Bit2To1 Shift(ALUB, ALUSrcResult, EX_SignExtend_10_6, shift);
     ALU32Bit ALU(ALUCon, EX_Read1, ALUB, EX_ALUResult, EX_ZeroFlag);
@@ -103,7 +105,7 @@ module Data_Path(Reset, Clk);
     Adder branchadd(EX_BranchAddResult, shiftedImmeadiate, EX_PCAddResult);
      
         
-    EX_MEM EXMEM(EX_WBCtrl, EX_MEMCtrl, EX_PCAddResult, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResult, EX_WriteMemData, EX_WriteRegData,Clk,MEM_WB_Ctrl, M_BranchCon,M_MemRead, M_Branch, M_MemWrite, M_BNE,MEM_PCAddResult, M_BranchAddResult, M_ZeroFlag, M_ALUResult, M_WriteMemData,MEM_RegDst); 
+    EX_MEM EXMEM(EX_WBCtrl, EX_MEMCtrl, EX_PCAddResult, EX_BranchAddResult, EX_ZeroFlag, EX_ALUResult, EX_Read2, EX_WriteRegData,Clk, Reset, MEM_WB_Ctrl, M_BranchCon,M_MemRead, M_Branch, M_MemWrite, M_BNE,MEM_PCAddResult, M_BranchAddResult, M_ZeroFlag, M_ALUResult, M_WriteMemData,MEM_RegDst); 
 
     Mux1bit2To1 specBranch(BranchSatsified, M_ZeroFlag, M_ALUResult[0],  M_BranchCon);
     and testAnd( BEQ_sat,M_Branch, BranchSatsified);
@@ -115,8 +117,8 @@ module Data_Path(Reset, Clk);
     MEM_WB_RegFile MEMWB(Clk, MEM_WB_Ctrl, MEM_Read, MEM_PCAddResult, M_ALUResult, MEM_RegDst, WB_halfbyte, WB_MemToReg, WB_RegWrite, WB_PCAddResult, WB_Read, WB_ALUResult, WB_RegDst);
     SignExtension_8 lb(WB_Read[7:0], loadByte);
     SignExtension lh(WB_Read[15:0], loadHalf);
-    Mux32Bit2To1 lhlb(lhlbResult, loadByte, loadHalf, WB_halfbyt);
-    Mux32Bit4To1 MemtoReg(WriteData, WB_Read, WB_ALUResult,inC,inD, WB_MemToReg);
+    Mux32Bit2To1 lhlb(lhlbResult, loadByte, loadHalf, WB_halfbyte);
+    Mux32Bit4To1 MemtoReg(WriteData, WB_Read, WB_ALUResult,lhlbResult,WB_PCAddResult, WB_MemToReg);
 
    
 endmodule
