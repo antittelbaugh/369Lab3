08c6d7be1b0ea57bc437f05178e407525c2efee8
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2022-10-23 13:30:50 -0700

    Update ID_EX_RegFile.v

--- ID_EX_RegFile.v
+++ ID_EX_RegFile.v
@@ -10,16 +10,21 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module IDEXReg(Clk, Reset, ID_WB_Ctrl, ID_MEM_Ctrl, ID_PCAddResult, ID_EX_Ctrl, ID_SignExtend, ID_SignExtend_10_6, ID_Read1, ID_Read2, ID_Instruction16_20, ID_Instruction5_11, EX_WBCtrl, EX_MEMCtrl, EX_RegDst, EX_ALUOp, EX_ALUSrc, EX_halfbyte, EX_PCAddResult, EX_Read1, EX_Read2, EX_SignExtend,EX_SignExtend_10_6,EX_Instruction16_20, EX_Instruction5_11);
+module IDEXReg(Clk, Reset, ID_WB_Ctrl, ID_MEM_Ctrl, ID_PCAddResult, ID_EX_Ctrl, ID_SignExtend, ID_SignExtend_10_6, ID_Read1, ID_Read2, ID_Instruction16_20, ID_Instruction5_11, EX_WBCtrl, EX_MEMCtrl, EX_RegDst, EX_ALUOp, EX_ALUSrc, EX_halfbyte, EX_PCAddResult, EX_Read1, EX_Read2, EX_SignExtend,EX_SignExtend_10_6,EX_Instruction16_20, EX_Instruction5_11, ID_jump, EX_jump, ID_offset, EX_offset);
     
     input Clk;
     input Reset;
+    input ID_jump;
+    input [25:0] ID_offset;
     input [3:0] ID_WB_Ctrl;
     input  [4:0] ID_MEM_Ctrl;
     input  [8:0] ID_EX_Ctrl;
     input  [31:0] ID_PCAddResult, ID_SignExtend_10_6, ID_SignExtend, ID_Read1, ID_Read2;
     input [4:0]  ID_Instruction16_20, ID_Instruction5_11;
 
+
+    output reg EX_jump;
+    output reg [25:0] EX_offset;
     output reg [3:0] EX_WBCtrl;
     output reg [4:0] EX_MEMCtrl;
     output reg [1:0] EX_RegDst;
@@ -33,7 +38,8 @@ module IDEXReg(Clk, Reset, ID_WB_Ctrl, ID_MEM_Ctrl, ID_PCAddResult, ID_EX_Ctrl, 
 always @(posedge Clk) begin
     if (Reset == 1) begin
         EX_WBCtrl <= 0;
-
+        EX_jump <= 0;
+        EX_offset <= 0;
     EX_MEMCtrl <= 0;
     
     EX_RegDst <= 0;
@@ -51,7 +57,8 @@ always @(posedge Clk) begin
     end
     else begin
     EX_WBCtrl <= ID_WB_Ctrl;
-
+    EX_jump <= ID_jump;
+        EX_offset <= ID_offset;
     EX_MEMCtrl <= ID_MEM_Ctrl;
     
     EX_RegDst <= ID_EX_Ctrl[1:0];
