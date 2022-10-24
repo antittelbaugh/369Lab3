fbeff481655171bc161701e7e3c3733ab1b4a327
mariow-ops <104529480+mariow-ops@users.noreply.github.com>
2022-10-23 20:53:17 -0700

    Update Controller_tb.v

--- Controller_tb.v
+++ Controller_tb.v
@@ -18,45 +18,45 @@ wire ID;
 
 	initial begin
 
-	OpCode <= 6'b000000
+	OpCode <= 6'b000000;
   	#100
-	OpCode <= 6'b000001
+	OpCode <= 6'b000001;
   	#100
-	OpCode <= 6'b000010
+	OpCode <= 6'b000010;
   	#100
-	OpCode <= 6'b000011
+	OpCode <= 6'b000011;
   	#100
-	OpCode <= 6'b000100
+	OpCode <= 6'b000100;
   	#100
-	OpCode <= 6'b000101
+	OpCode <= 6'b000101;
   	#100
-	OpCode <= 6'b000110
+	OpCode <= 6'b000110;
   	#100
-	OpCode <= 6'b000111
+	OpCode <= 6'b000111;
   	#100
-	OpCode <= 6'b001000
+	OpCode <= 6'b001000;
   	#100
-	OpCode <= 6'b001010
+	OpCode <= 6'b001010;
   	#100
-	OpCode <= 6'b001100
+	OpCode <= 6'b001100;
   	#100
-	OpCode <= 6'b001101
+	OpCode <= 6'b001101;
   	#100
-	OpCode <= 6'b001110
+	OpCode <= 6'b001110;
   	#100
-	OpCode <= 6'b011100
+	OpCode <= 6'b011100;
   	#100
-	OpCode <= 6'b100000
+	OpCode <= 6'b100000;
   	#100
-	OpCode <= 6'b100001
+	OpCode <= 6'b100001;
   	#100
-	OpCode <= 6'b100011
+	OpCode <= 6'b100011;
   	#100
-	OpCode <= 6'b101000
+	OpCode <= 6'b101000;
   	#100
-	OpCode <= 6'b101001
+	OpCode <= 6'b101001;
   	#100
-	OpCode <= 6'b101011
+	OpCode <= 6'b101011;
   	#100
 
 	end
