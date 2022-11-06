073fb8c3e44add7173b277a0f38ff24b1fc5f4fd
juliaotto <89600341+juliaotto@users.noreply.github.com>
2022-11-05 21:20:25 -0700

    Update HazardDetectionUnit.v

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -29,41 +29,75 @@ module HazardDetectionUnit();
 			PCWrite<=0;
 			IF_ID_Write<=0;
 			control<=0;
+			IF_ID_flush<=0;
 		}
-		else{
-			IF_ID_flush<=1;
+		else if(jumpReg==1){
+			PCWrite <= 1;
+            IF_ID_Write <= 0;
+            control <= 1;
+			IF_ID_flush <= 1;
 		}
 		if(jump==1){
-			IF_ID_flush<=1;
+			PCWrite <= 1;
+            IF_ID_Write <= 0;
+            control <= 1;
+			IF_ID_flush <= 1;
 		}
 		if (branch == 1){
-			IF_ID_flush<=1;
+			PCWrite <= 1;
+            IF_ID_Write <= 0;
+            control <= 1;
+			IF_ID_flush <= 1;
 		}
 
 		if((MEM_WB_RegWrite==1)&&((MEM_WB_Rd==IF_ID_Rs) || (MEM_WB_Rd==IF_ID_Rt)){
 			PCWrite<=0;
 			IF_ID_Write<=0;
 			control<=0;
+			IF_ID_flush<=0;
 		}
-		if((MEM_WB_MemToReg==1)&&((MEM_WB_Rd==IF_ID_Rs) || (MEM_WB_Rd==IF_ID_Rt)){
-			PCWrite<=0;
-			IF_ID_Write<=0;
-			control<=0;
+		else{
+			PCWrite<=1;
+			IF_ID_Write<=1;
+			control<=1;
+			IF_ID_flush<=0;
 		}
+		
 		if((EX_MEM_MemRead==1) && ((EX_MEM_Rd==IF_ID_Rs)||(EX_MEM_Rd==IF_ID_Rt))){
 			PCWrite<=0;
 			IF_ID_Write<=0;
 			control<=0;
+			IF_ID_flush<=0;
+		}
+		else{
+			PCWrite<=1;
+			IF_ID_Write<=1;
+			control<=1;
+			IF_ID_flush<=0;
 		}
 		if((ID_EX_RegWrite==1) &&((ID_EX_Rd==IF_ID_Rs)||(ID_EX_Rd==IF_ID_Rt)){
 			PCWrite<=0;
 			IF_ID_Write<=0;
 			control<=0;
+			IF_ID_flush<=0;
+		}
+		else{
+			PCWrite<=1;
+			IF_ID_Write<=1;
+			control<=1;
+			IF_ID_flush<=0;
 		}
 		if((ID_EX_MemWrite==1) &&((ID_EX_Rd==IF_ID_Rs)||(ID_EX_Rd==IF_ID_Rt)){
 			PCWrite<=0;
 			IF_ID_Write<=0;
 			control<=0;
+			IF_ID_flush<=0;
+		}
+		else{
+			PCWrite<=1;
+			IF_ID_Write<=1;
+			control<=1;
+			IF_ID_flush<=0;
 		}
 
 endmodule
