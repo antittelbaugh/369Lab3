701aea0d4c227881d019739ba4951a98ea44aea7
juliaotto <89600341+juliaotto@users.noreply.github.com>
2022-11-06 19:50:32 -0700

    Create Mux2Bit2To1.v

---
+++ Mux2Bit2To1.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// ECE369 - Computer Architecture
+// 
+// Module - Mux32Bit2To1.v
+// Description - Performs signal multiplexing between 2 32-Bit words.
+////////////////////////////////////////////////////////////////////////////////
+
+module Mux2Bit2To1(out, inA, inB, sel);
+
+  output reg [1:0] out;
+    
+  input [1:0] inA;
+  input [1:0] inB;
+
+    input [1:0] sel;
+
+    always @(*) begin
+    if(sel==0)begin
+        out <= inA;
+    end
+    else if( sel ==1)begin
+        out <= inB;
+    end
+    else begin
+        out <= inC;
+    end
+        
+end
+
+
+endmodule
