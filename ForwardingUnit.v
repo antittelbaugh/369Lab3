e4bea1fb3845c9ac3d9cc9d8743815c07c64f6a5
juliaotto <89600341+juliaotto@users.noreply.github.com>
2022-12-04 20:04:18 -0700

    Update ForwardingUnit.v

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -1,76 +1,70 @@
 `timescale 1ns / 1ps
 
-module ForwardingUnit(ID_RegRs, ID_RegRt, EX_RegRs, EX_RegRt, MEM_Rd, MEM_RegWrite, WB_Rd, WB_RegWrite, EX_ReadData1MEM, EX_ReadData2MEM, EX_ReadData1WB, EX_ReadData2WB, ID_ReadData1MEM, ID_ReadData2MEM, ID_ReadData1WB, ID_ReadData2WB);
-
+module ForwardingUnit(ID_RegRs, ID_RegRt, EX_RegRs, EX_RegRt, MEM_Rd, MEM_RegWrite, WB_Rd, WB_RegWrite, ForwardA, ForwardB);
 	input [4:0] ID_RegRs, ID_RegRt, EX_RegRs, EX_RegRt, MEM_Rd, WB_Rd;
 	input MEM_RegWrite, WB_RegWrite;
 
-	output ID_ReadData1MEM, ID_ReadData2MEM;
-	output ID_ReadData1WB, ID_ReadData2WB; 
-	output EX_ReadData1MEM, EX_ReadData2MEM; 
-	output EX_ReadData1WB, EX_ReadData2WB; 
+	output [1:0] ForwardA;
+	output [1:0] ForwardB;
+
+
 
 	
 	
 	always @(ID_RegRs, ID_RegRt, EX_RegRs, EX_RegRt, MEM_Rd, WB_Rd, MEM_RegWrite, WB_RegWrite) begin
-            EX_ReadData1MEM <= 0;
-            EX_ReadData2MEM <= 0;
-            ID_ReadData1 <= 0;
-            ID_ReadData2 <= 0;
-
+           
+			ForwardA<= 2'b00;
+			ForwardB<=2b'00;
+	
         //FORWARDING TO EXECUTE RS
 		if (EX_RegRs == MEM_Rd && MEM_RegWrite == 1) begin
-			EX_ReadData1MEM <= 1; //forward from MEM to EX
-			EX_ReadData1WB <= 0; 
+
+			ForwardA <= 2'b01;
+
 		end 
 		else if (EX_RegRs == WB_Rd && WB_RegWrite == 1) begin
-			EX_ReadData1WB <= 1; //forward from WB to EX
-			EX_ReadData1MEM <= 0;
+
+			ForwardA <= 2'b10;
+
 		end 
 		else begin
-			EX_ReadData1MEM <= 0;
-			EX_ReadData1WB <= 0; 
+
+			ForwardA <= 2'b00;
 		end
 
 		//FORWARDING TO EXECUTE RT
 		if (EX_RegRt == MEM_Rd && MEM_RegWrite == 1) begin
-			EX_ReadData2MEM <= 1;
-      EX_ReadData2WB <= 0; 
-    end 
-    else if (EX_RegRt == WB_Rd && WB_RegWrite == 1) begin
-			EX_ReadData2WB <= 1;
-			EX_ReadDate2MEM <= 0;
+
+			ForwardB <= 2'b01;
 		end 
-    else begin
-			EX_ReadData2MEM <= 0;
-			EX_ReadData2WB <= 0;
+		else if (EX_RegRt == WB_Rd && WB_RegWrite == 1) begin
+
+			ForwardB <= 2'b10
+		end 
+		else begin
+
+			ForwardB <= 2'b00;
 		end
 		
 		// FORWARDING TO DECODE
 		if (ID_RegRs == MEM_Rd && MEM_RegWrite == 1 && MEM_Rd != 0) begin
-			ID_ReadData1MEM <= 1;
-			ID_ReadData1WB<= 0;
-    end 
-    else if (ID_RegRs == WB_Rd && WB_RegWrite == 1 && WB_Rd != 0) begin
-      ID_ReadData1WB <= 1;
-			ID_ReadData1MEM <= 0;
+			ForwardA <= 2'b01;
+        	end 
+		else if (ID_RegRs == WB_Rd && WB_RegWrite == 1 && WB_Rd != 0) begin
+			ForwardA <= 2'b10;
 		end 
-    else begin
-			ID_ReadData1MEM <= 0;
-      ID_ReadData1WB <= 0;
+		else begin
+			ForwardA <= 2'b00;
 		end
 		
 		if (ID_RegRt == MEM_Rd && MEM_RegWrite == 1 && MEM_Rd != 0) begin
-			ID_ReadData2MEM <= 1;
-      ID_ReadData2WB<= 0;
-    end 
-    else if (ID_RegRt == WB_Rd && WB_RegWrite == 1 && WB_Rd != 0) begin
-      ID_ReadData2WB <= 1;
-			ID_ReadData2MEM <= 0;
+			ForwardB <= 2'b01;
+        	end 
+		else if (ID_RegRt == WB_Rd && WB_RegWrite == 1 && WB_Rd != 0) begin
+			ForwardB <= 2'b10;
 		end 
-    else begin
-			ID_ReadData2MEM<= 0;
-      ID_ReadData2WB <= 0;
+		else begin
+			ForwardB <= 2'b00;
 		end
 		
 
